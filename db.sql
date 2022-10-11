2613247f7d252431f8c03964fcd58a048743f2db
Luigibro26 <43809254+Luigibro26@users.noreply.github.com>
2022-10-11 10:40:40 +1030

    Update db.sql

--- db.sql
+++ db.sql
@@ -7,38 +7,14 @@ USE Group2030;
 
 CREATE TABLE user_form(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    UName varchar(100),
-    FName varchar(100),
-    Password varchar(100),
-    Email varchar(100),
-    Dob DATE
-) AUTO_INCREMENT = 1;
-
-
-CREATE TABLE ItemN(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name varchar(100),
-    Cost int,
-    Ship int,
-    Category varchar(100),
-    itemCon varchar(100),
-    Description text
-) AUTO_INCREMENT = 1;
-
-
-CREATE TABLE ItemU(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name varchar(100),
-    Cost int,
-    Ship int,
-    Category varchar(100),
-    itemCon varchar(100),
-    Description text
+    UName varchar(255),
+    FName varchar(255),
+    LName varchar(128),
+    Password varchar(128),
+    Email varchar(100)
 ) AUTO_INCREMENT = 1;
 
 
 CREATE user IF NOT EXISTS dbadmin@localhost;
 GRANT all privileges ON Group2030.user_form TO dbadmin@localhost;
-GRANT all privileges ON Group2030.ItemN TO dbadmin@localhost;
-GRANT all privileges ON Group2030.ItemU TO dbadmin@localhost;
 
