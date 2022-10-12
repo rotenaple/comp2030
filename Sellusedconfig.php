3057f0ec7347f8b72e0c6754a94b9f3bfe4257d8
Luigibro26 <43809254+Luigibro26@users.noreply.github.com>
2022-10-12 20:29:14 +1030

    More Sell Db

--- Sellusedconfig.php
+++ Sellusedconfig.php
@@ -2,8 +2,8 @@
 
 require_once "ACCdatabase.php";
 
-$sql = "INSERT INTO user_form (UName, FName, Email, date_of_birth, password)
-        VALUES (?, ?, ?, ?, ?)";
+$sql = "INSERT INTO user_form (Name, Cost, Ship, Amount, Category, itemCon, Description)
+        VALUES (?, ?, ?, ?, ?, ?)";
 
 $stmt = mysqli_stmt_init($conn);
 
@@ -11,37 +11,17 @@ if( ! $stmt->prepare($sql) ) {
     die("Error within SQL " . $conn->error);
 }
 
-$stmt->bind_param("sssss",
-                $_POST["Uname"],
-                $_POST["Fname"],
-                $_POST["email"],
-                $_POST["date_of_birth"],
-                $password_hash);
-
-                CREATE TABLE user_form(
-                    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                    UName varchar(255),
-                    FName varchar(255),
-                    Password varchar(128),
-                    date_of_birth DATE,
-                    Email varchar(100)
-                ) AUTO_INCREMENT = 1;
-                
-
-                CREATE TABLE ItemU(
-                    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                    Name varchar(100),
-                    Cost int,
-                    Ship int,
-                    Category varchar(100),
-                    itemCon varchar(100),
-                    Description text
-                ) AUTO_INCREMENT = 1;
-
-
+$stmt->bind_param("ssssss",
+                $_POST["name"],
+                $_POST["price"],
+                $_POST["ship"],
+                $_POST["amount"],
+                $_POST["productcat"],
+                $_POST["isUsed"],
+                $_POST["description"]);
 
 if ($stmt->execute()) { 
-    header("Location: RegisterComplete.html");
+    header("Location: SellComplete.html");
 } else {
     die($mysqli->error);
 }
