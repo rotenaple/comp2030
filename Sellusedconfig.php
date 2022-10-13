0532fb6c1df3b09d1441ed90529346c9b2d8c7f1
person782 <114961542+person782@users.noreply.github.com>
2022-10-13 14:47:37 +0300

    Update Sellusedconfig.php

--- Sellusedconfig.php
+++ Sellusedconfig.php
@@ -2,7 +2,7 @@
 if (isset($_POST["create"])) {
     require_once "ACCdatabase.php";
 
-    $sql = "INSERT INTO itemu (Name, Cost, Ship, Amount, Category, itemCon, Description)
+    $sql = "INSERT INTO itemu (PName, Cost, Ship, Amount, Category, itemCon, Description)
         VALUES (?, ?, ?, ?, ?, ?, ?)";
 
     $stmt = mysqli_stmt_init($conn);
@@ -11,14 +11,14 @@ if (isset($_POST["create"])) {
        die("Error within SQL " . $conn->error);
     }
 
-    $stmt->bind_param("ssssss",
-                $_POST["name"],
-                $_POST["price"],
-                $_POST["ship"],
-                $_POST["amount"],
-                $_POST["productcat"],
-                $_POST["isUsed"],
-                $_POST["description"]);
+    $stmt->bind_param("sssssss",
+                htmlspecialchars($_POST["name"]),
+                htmlspecialchars($_POST["price"]),
+                htmlspecialchars($_POST["ship"]),
+                htmlspecialchars($_POST["amount"]),
+                htmlspecialchars($_POST["productcat"]),
+                htmlspecialchars($_POST["isUsed"]),
+                htmlspecialchars($_POST["description"]);
 
     if ($stmt->execute()) { 
         header("Location: SellComplete.html");
