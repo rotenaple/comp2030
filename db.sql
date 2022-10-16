4143c3b101ec3ad1e3ee440a766680c1b8a2797b
person782 <114961542+person782@users.noreply.github.com>
2022-10-16 16:42:52 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -6,13 +6,12 @@ CREATE DATABASE Group2030;
 USE Group2030;
 
 CREATE TABLE user_form(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    UName varchar(255),
+    UName varchar(255) PRIMARY KEY,
     FName varchar(255),
     Password varchar(128),
     date_of_birth DATE,
     Email varchar(100)
-)
+);
 
 CREATE TABLE ItemN(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -24,7 +23,7 @@ CREATE TABLE ItemN(
     itemCon varchar(100),
     Description text,
     posted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE ItemU(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -36,33 +35,34 @@ CREATE TABLE ItemU(
     itemCon varchar(100),
     Description text,
     posted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE user_cart(
-    cart_id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    UName varchar(255),
+    cart_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     PName varchar(100),
     Cost int,
     Ship int,
-    Quantity int, 
-    UName varchar(255) FOREIGN KEY REFERENCES user_form(UName)
-)
+    Quantity int,
+    FOREIGN KEY (UName) REFERENCES user_form(UName)
+);
+
 CREATE TABLE paid(
-    transact_id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    UName varchar(255),
+    transact_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     FName varchar(255),
     LName varchar(255),
     ccnum varchar(16),
     ccvv varchar(3),
     ccmonth DATE,
     Total int,
-    UName varchar(255) FOREIGN KEY REFERENCES user_form(UName),
-    updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
-
-
-
+    updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (UName) REFERENCES user_form(UName)
+);
 
 CREATE user IF NOT EXISTS dbadmin@localhost;
 GRANT all privileges ON Group2030.user_form TO dbadmin@localhost;
 GRANT all privileges ON Group2030.ItemN TO dbadmin@localhost;
 GRANT all privileges ON Group2030.ItemU TO dbadmin@localhost;
+
 
